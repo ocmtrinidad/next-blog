13fbe59bfa51faffacb464cd2a855e4986946b0a
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-10 13:16:42 +0800

    Created prisma function to add new user

---
+++ src/prisma-db.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@/generated/prisma";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export const addUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return;
+};
+
+export default prisma;
