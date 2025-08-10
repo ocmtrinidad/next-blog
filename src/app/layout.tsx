fd71b4b2862885236ec015db239ecd3868fbf9af
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-10 13:15:54 +0800

    Imported <AuthSessionProvider> and <Navbar>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import AuthSessionProvider from "./(components)/AuthSessionProvider";
+import Navbar from "./(components)/Navbar";
 
 export const metadata: Metadata = {
   title: "Next Blog",
@@ -13,7 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>
+          <Navbar />
+          <AuthSessionProvider>{children}</AuthSessionProvider>
+        </div>
+      </body>
     </html>
   );
 }
