887bb73c971e93eafb8dc44b338968cd59f040cb
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-11 12:39:46 +0800

    Updated

---
+++ src/app/controllers/users.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { addUser } from "@/models/users";
+import { redirect } from "next/navigation";
+
+type UserErrors = {
+  name?: string;
+  email?: string;
+  password?: string;
+};
+
+export type UserFormState = {
+  errors: UserErrors;
+};
+
+export const createUser = async (prevState: FormData, formData: FormData) => {
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const errors: UserErrors = {};
+  if (!name) {
+    errors.name = "Name is required";
+  }
+
+  if (!email) {
+    errors.email = "Email is required";
+  }
+
+  if (!password) {
+    errors.password = "Password is required";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  await addUser(name, email, password);
+  redirect("/");
+};
