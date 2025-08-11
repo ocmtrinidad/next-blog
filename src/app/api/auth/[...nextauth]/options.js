35fe07e8f26ebefcbcf5ab75d358bf84112ae540
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-11 13:00:23 +0800

    Updated import

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,4 @@
-import { getUserByEmail } from "@/models/users";
+import { getUserByEmail } from "@/models/usersModels";
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
