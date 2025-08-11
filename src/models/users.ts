77e99232b51e56132795b7042836d67793b69d11
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-11 12:41:25 +0800

    Created UserType type.

---
+++ src/models/users.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@/generated/prisma";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export type UserType = {
+  id: string;
+  name: string;
+  email: string;
+  image?: string;
+  role?: string;
+};
+
+export const addUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+};
+
+export const getUserByEmail = async (email: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (user) {
+    return user;
+  }
+  return null;
+};
+
+export default prisma;
