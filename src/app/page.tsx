ce7b1242b478e59d7b58731b3e2c40b7c0e7e3e9
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-12 13:47:14 +0800

    Imported getPosts(), then display it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import { getPosts } from "@/models/postsModels";
+import Image from "next/image";
+
+export default async function Home() {
+  const posts = await getPosts();
+
   return (
     <>
-      <div>HELLO WORLD</div>
+      {posts.map((post) => (
+        <div key={post.id} className="border flex justify-between">
+          <div className="p-4 mb-4">
+            <p>{post.title}</p>
+            <p>{post.content}</p>
+          </div>
+          <Image
+            src={post.image}
+            alt={post.title}
+            width={250}
+            height={250}
+            priority={true}
+          />
+        </div>
+      ))}
     </>
   );
 }
