17d9c92459418ee95946f7c86f403f1fbdbbf65e
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-12 12:03:57 +0800

    Added cloudinary uploading

--- src/models/postsModels.ts
+++ src/models/postsModels.ts
@@ -1,21 +1,43 @@
 import { PrismaClient } from "@/generated/prisma";
+import { v2 as cloudinary } from "cloudinary";
 
 const prisma = new PrismaClient();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export const addPost = async (
   id: string,
   title: string,
   content: string,
-  image: string
+  image: File
 ) => {
-  return await prisma.post.create({
-    data: {
-      authorId: id,
-      title,
-      content,
-      image,
-    },
-  });
+  const arrayBuffer = await image.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  cloudinary.uploader
+    .upload_stream(
+      { resource_type: "image", folder: "next-blog" },
+      async (error, result) => {
+        if (error) {
+          throw new Error("Failed to upload image");
+        }
+        if (!result) {
+          throw new Error("No result returned from Cloudinary upload");
+        }
+        return await prisma.post.create({
+          data: {
+            authorId: id,
+            title,
+            content,
+            image: result.secure_url,
+          },
+        });
+      }
+    )
+    .end(buffer);
 };
 
 export default prisma;
