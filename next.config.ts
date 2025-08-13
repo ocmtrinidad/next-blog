4d0da9dc8155497f82ced7593b67f47f48acb892
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-13 10:37:22 +0800

    Updated

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      new URL(
-        "https://res.cloudinary.com/dihu84tof/image/upload/v1754970842/next-blog/**"
-      ),
-    ],
+    remotePatterns: [new URL("https://res.cloudinary.com/**")],
   },
 };
 
