f6e7dbf222ad95e0e3edc3f70b7b3310ec582681
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-13 11:10:10 +0800

    Added confirmPassword input

--- src/app/controllers/usersControllers.ts
+++ src/app/controllers/usersControllers.ts
@@ -7,16 +7,20 @@ type UserErrors = {
   name?: string;
   email?: string;
   password?: string;
+  confirmPassword?: string;
 };
 
 export type UserFormState = {
   errors: UserErrors;
+  name: string;
+  email: string;
 };
 
 export const createUser = async (prevState: FormData, formData: FormData) => {
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
+  const confirmPassword = formData.get("confirmPassword") as string;
 
   const errors: UserErrors = {};
   if (!name) {
@@ -31,8 +35,12 @@ export const createUser = async (prevState: FormData, formData: FormData) => {
     errors.password = "Password is required";
   }
 
+  if (password !== confirmPassword && password.length && confirmPassword) {
+    errors.confirmPassword = "Confirm Password must match your password";
+  }
+
   if (Object.keys(errors).length > 0) {
-    return { errors };
+    return { errors, name, email };
   }
 
   await addUser(name, email, password);
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,6 +6,8 @@ import { createUser, UserFormState } from "../controllers/usersControllers";
 export default function RegisterPage() {
   const initialState: UserFormState = {
     errors: {},
+    name: "",
+    email: "",
   };
 
   const [state, formAction, isPending] = useActionState(
@@ -14,43 +16,71 @@ export default function RegisterPage() {
   );
 
   return (
-    <>
-      <h2>Register Page</h2>
-      <form action={formAction}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
-          {state?.errors.name && (
-            <p className="text-red-500">{state.errors.name}</p>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="username">Email:</label>
-          <input type="email" name="email" id="email" />
-          {state?.errors.email && (
-            <p className="text-red-500">{state.errors.email}</p>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" />
-          {state?.errors.password && (
-            <p className="text-red-500">{state.errors.password}</p>
-          )}
-        </div>
-
-        {isPending ? (
-          <button className="bg-gray-500 px-4 py-2 rounded cursor-pointer">
-            Submitting...
-          </button>
-        ) : (
-          <button className="bg-blue-500 px-4 py-2 rounded cursor-pointer">
-            Register
-          </button>
+    <form action={formAction} className="flex flex-col gap-4">
+      <h2 className="text-xl font-bold">Register Page</h2>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="border rounded p-2 w-full"
+          defaultValue={state.name}
+        />
+        {state?.errors.name && (
+          <p className="text-red-500">{state.errors.name}</p>
         )}
-      </form>
-    </>
+      </div>
+
+      <div>
+        <label htmlFor="username">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="border rounded p-2 w-full"
+          defaultValue={state.email}
+        />
+        {state?.errors.email && (
+          <p className="text-red-500">{state.errors.email}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="border rounded p-2 w-full"
+        />
+        {state?.errors.password && (
+          <p className="text-red-500">{state.errors.password}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="confirmPassword">Confirm Your Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          className="border rounded p-2 w-full"
+        />
+        {state?.errors.confirmPassword && (
+          <p className="text-red-500">{state.errors.confirmPassword}</p>
+        )}
+      </div>
+
+      {isPending ? (
+        <button className="bg-gray-500 px-4 py-2 rounded cursor-pointer">
+          Submitting...
+        </button>
+      ) : (
+        <button className="bg-blue-500 px-4 py-2 rounded cursor-pointer">
+          Register
+        </button>
+      )}
+    </form>
   );
 }
