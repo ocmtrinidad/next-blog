24cc750c46d9a5e05f1e151b83da84e5d6e735e8
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-14 13:09:32 +0800

    Created page to display specific posts

---
+++ src/app/post/[id]/page.tsx
@@ -0,0 +1,32 @@
+import PostHeader from "@/app/(components)/PostHeader";
+import { getPost, Post } from "@/models/postsModels";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function PostId({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const post: Post | null = await getPost(id);
+
+  if (!post) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex flex-col p-4 gap-4 border rounded">
+      <PostHeader post={post} />
+      <Image
+        src={post.image}
+        alt={post.title}
+        width={400}
+        height={400}
+        priority={true}
+        className="self-center"
+      />
+      <p>{post.content}</p>
+    </div>
+  );
+}
