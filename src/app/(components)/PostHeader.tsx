6487a7573dfa33374b6640bf38cb9fbfda1874cd
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-14 13:16:50 +0800

    Added <Link/> to user page

--- src/app/(components)/PostHeader.tsx
+++ src/app/(components)/PostHeader.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function PostHeader({ post }: { post: Post }) {
   return (
-    <div>
+    <div className="flex flex-col gap">
       <Link href={`/post/${post.id}`} className="text-2xl font-bold">
         {post.title}
       </Link>
-      <p>By: {post.author.name}</p>
+      <Link href={`/user/${post.author.id}`}>By: {post.author.name}</Link>
       <p>{post.createdAt.toDateString()}</p>
     </div>
   );
