2ca827535f21e23bacd27056fd837be137336544
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-20 13:50:28 +0800

    Imported <SearchPost /> functionality

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -3,17 +3,25 @@ import { options } from "../api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import { getPostsByAuthor } from "@/models/postsModels";
 import PostList from "../(components)/PostList";
+import SearchPost from "../(components)/SearchPost";
 
-export default async function ProfilePage() {
+export default async function ProfilePage({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
   const session = await getServerSession(options);
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/profile");
   }
 
-  const posts = await getPostsByAuthor(session.user.id);
+  const { query } = await searchParams;
+
+  const posts = await getPostsByAuthor(session.user.id, query);
   return (
     <>
       <h1 className="mb-4 text-2xl font-bold">Your Posts</h1>
+      <SearchPost route={"/my-posts"} placeholder="Search Your Posts" />
       <PostList posts={posts} />
     </>
   );
