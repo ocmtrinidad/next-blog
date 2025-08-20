1102206f021b182010f35c46b4edf1ebd4a70ecf
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-20 13:20:11 +0800

    Added dynamic Form action

--- src/app/(components)/SearchPost.tsx
+++ src/app/(components)/SearchPost.tsx
@@ -1,14 +1,14 @@
 import Form from "next/form";
 import BlueButton from "./BlueButton";
 
-export default function SearchPost() {
+export default function SearchPost({ route }: { route: string }) {
   return (
-    <Form action={"/"} className="flex mb-4">
+    <Form action={route} className="flex mb-4">
       <input
         type="text"
         name="query"
         placeholder="Search Posts"
-        className="border flex-1 rounded p-2"
+        className="border border-r-0 flex-1 rounded rounded-r-none rounded-br-none p-2"
       />
       <BlueButton>Search</BlueButton>
     </Form>
