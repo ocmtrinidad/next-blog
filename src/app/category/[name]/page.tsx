47bf2fd570fee5ef5e88516c58801118268f94dd
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-20 13:20:38 +0800

    Imported <SearchPost/>

--- src/app/category/[name]/page.tsx
+++ src/app/category/[name]/page.tsx
@@ -2,25 +2,30 @@ import { getPostsByCategory, Post } from "@/models/postsModels";
 import PostList from "@/app/(components)/PostList";
 import CategoryList from "@/app/(components)/CategoryList";
 import { Category, getCategories } from "@/models/categoryModels";
+import SearchPost from "@/app/(components)/SearchPost";
 
 export default async function CategoryPosts({
   params,
+  searchParams,
 }: {
   params: Promise<{ name: string }>;
+  searchParams: Promise<{ query?: string }>;
 }) {
   const { name } = await params;
+  const { query } = await searchParams;
   const [posts, categories]: [Post[], Category[]] = await Promise.all([
-    await getPostsByCategory(name),
+    await getPostsByCategory(name, query),
     await getCategories(),
   ]);
 
   return (
     <>
       <CategoryList categories={categories} selectedCategoryName={name} />
+      <SearchPost route={`/category/${name}`} />
       {!posts || !posts.length ? (
         <p className="text-center">No posts found in this category.</p>
       ) : (
-        <PostList posts={posts}/>
+        <PostList posts={posts} />
       )}
     </>
   );
