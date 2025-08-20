b6a540eb144b2377447959837cdfe2c613dae1a3
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-20 13:21:13 +0800

    Added route prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home({
   return (
     <>
       <CategoryList categories={categories} selectedCategoryName={null} />
-      <SearchPost />
+      <SearchPost route={"/"} />
       <PostList posts={posts} />
     </>
   );
