048c5c9717fa100dbb45ce00fcf2689bfd157531
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-24 12:43:32 +0800

    removed session check

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,8 +1,5 @@
 import { getPostsByAuthor } from "@/models/postsModels";
 import PostList from "@/app/(components)/PostList";
-import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
 import { getUserById } from "@/models/usersModels";
 import SearchPost from "@/app/(components)/SearchPost";
 
@@ -15,31 +12,24 @@ export default async function UserPage({
 }) {
   const { id } = await params;
   const { query } = await searchParams;
-
-  const session = await getServerSession(options);
-
-  if (session && session.user.id === id) {
-    redirect("/my-posts");
-  } else {
-    const [posts, user] = await Promise.all([
-      getPostsByAuthor(id, query),
-      getUserById(id),
-    ]);
-    return (
-      <div className="flex gap-4">
-        <div className="w-2/5">
-          <h1 className="text-2xl font-bold">{user?.name}'s Profile Page</h1>
-          <p>{user?.bio}</p>
-        </div>
-
-        <div className="flex-1">
-          <SearchPost
-            route={`/user/${user?.id}`}
-            placeholder={`Search ${user?.name}'s Posts`}
-          />
-          <PostList posts={posts} />
-        </div>
+  const [posts, user] = await Promise.all([
+    getPostsByAuthor(id, query),
+    getUserById(id),
+  ]);
+  return (
+    <div className="flex gap-4">
+      <div className="w-2/5">
+        <h1 className="text-2xl font-bold">{user?.name}'s Profile Page</h1>
+        <p>{user?.bio}</p>
       </div>
-    );
-  }
+
+      <div className="flex-1">
+        <SearchPost
+          route={`/user/${user?.id}`}
+          placeholder={`Search ${user?.name}'s Posts`}
+        />
+        <PostList posts={posts} />
+      </div>
+    </div>
+  );
 }
