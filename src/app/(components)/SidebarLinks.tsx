f3f403906e352e2d7cef046851be49cac7b17c05
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-29 12:36:06 +0800

    Created <SidebarLinks /> component

---
+++ src/app/(components)/SidebarLinks.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import BlueButton from "./BlueButton";
+
+export default function SidebarLinks({
+  setShowSidebar,
+  url,
+  text,
+}: {
+  setShowSidebar: (show: boolean) => void;
+  url: string;
+  text: string;
+}) {
+  return (
+    <div onClick={() => setShowSidebar(false)}>
+      <Link href={url} className="px-4">
+        <BlueButton>{text}</BlueButton>
+      </Link>
+    </div>
+  );
+}
