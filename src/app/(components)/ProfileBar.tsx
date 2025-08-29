4c8c5033965cd39fc25e4865bb010fa11ac1254e
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-08-29 11:55:50 +0800

    Styling

--- src/app/(components)/ProfileBar.tsx
+++ src/app/(components)/ProfileBar.tsx
@@ -11,25 +11,39 @@ export default function ProfileBar({ userId }: { userId: string }) {
     <>
       {showBar && (
         <div
-          className="fixed top-0 right-0 z-50 border cursor-pointer"
+          className="fixed inset-0 z-40 bg-black opacity-50 cursor-pointer"
           onClick={() => setShowBar(false)}
-        >
-          <div className="flex flex-col gap-2 p-4 border w-fit bg-white">
-            <Link href={"/create-post"}>
-              <BlueButton>Create Post</BlueButton>
-            </Link>
-            <Link href={`/user/${userId}`}>
-              <BlueButton>My Posts</BlueButton>
-            </Link>
-            <Link href={"/profile"}>
-              <BlueButton>Profile</BlueButton>
-            </Link>
-            <Link href={"/api/auth/signout?callbackUrl=/"}>
-              <BlueButton>Logout</BlueButton>
-            </Link>
-          </div>
-        </div>
+        />
       )}
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-50 z-50 bg-[#333333] border-r transition-transform duration-300 ${
+          showBar ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex flex-col gap-2">
+          {/* x button */}
+          <button
+            className="self-end text-white cursor-pointer p-4"
+            onClick={() => setShowBar(false)}
+          >
+            &times;
+          </button>
+          <Link href={"/create-post"} className="px-4">
+            <BlueButton>Create Post</BlueButton>
+          </Link>
+          <Link href={`/user/${userId}`} className="px-4">
+            <BlueButton>My Posts</BlueButton>
+          </Link>
+          <Link href={"/profile"} className="px-4">
+            <BlueButton>Profile</BlueButton>
+          </Link>
+          <Link href={"/api/auth/signout?callbackUrl=/"} className="px-4">
+            <BlueButton>Logout</BlueButton>
+          </Link>
+        </div>
+      </div>
+
       <div onClick={() => setShowBar(true)}>
         <BlueButton>Menu</BlueButton>
       </div>
