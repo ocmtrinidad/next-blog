2112b04f404bb342a2f0996a25eff7f49f05eff8
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-02 11:35:50 +0800

    Created addLike() and removeLike()

---
+++ src/models/likeModels.ts
@@ -0,0 +1,25 @@
+import prisma from "../../lib/prisma";
+
+export type Like = {
+  id: string;
+  userId: string;
+  postId: string;
+};
+
+export const addLike = async (userId: string, postId: string) => {
+  return await prisma.like.create({
+    data: {
+      userId,
+      postId,
+    },
+  });
+};
+
+export const removeLike = async (userId: string, postId: string) => {
+  return await prisma.like.deleteMany({
+    where: {
+      userId,
+      postId,
+    },
+  });
+};
