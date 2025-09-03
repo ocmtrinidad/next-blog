bbab06f0cbe9c7b549fb1e4fd411a8ead7d48028
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-03 14:29:56 +0800

    Created deleteComment()

--- src/app/(components)/CommentsSection.tsx
+++ src/app/(components)/CommentsSection.tsx
@@ -25,19 +25,43 @@ export default function CommentsSection({
       redirect("/api/auth/signin?callbackUrl=/");
     }
 
-    const response = await fetch(`/api/posts/${post.id}/comments`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        authorId: user.id,
-        content: e.currentTarget.comment.value,
-      }),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      setComments(data);
+    try {
+      const response = await fetch(`/api/posts/${post.id}/comments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          authorId: user.id,
+          content: e.currentTarget.comment.value,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setComments(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteComment = async (id: string) => {
+    try {
+      const response = await fetch(`/api/posts/${post.id}/comments`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          commentId: id,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setComments(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -72,7 +96,26 @@ export default function CommentsSection({
               <p>{comment.author.name}</p>
             </Link>
             <p>{comment.content}</p>
-            <p>{comment.createdAt.toDateString()}</p>
+            <div className="flex gap-2">
+              <p>{new Date(comment.createdAt).toDateString()}</p>
+              {user && user.id === comment.author.id && (
+                <button
+                  onClick={() => deleteComment(comment.id)}
+                  className="cursor-pointer bg-red-600 hover:bg-red-800 rounded"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-6"
+                  >
+                    <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+                </button>
+              )}
+            </div>
           </div>
         ))}
       </div>
