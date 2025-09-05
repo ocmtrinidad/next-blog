136b4ed9946d320b28b3b41a7f0408bf0e669f1e
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-05 13:00:08 +0800

    Created controllers for comments

---
+++ src/app/controllers/commentsControllers.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import {
+  addComment,
+  deleteComment,
+  getCommentsByPostId,
+} from "@/models/commentModels";
+
+export const getAllCommentsByPost = async (postId: string) => {
+  return await getCommentsByPostId(postId);
+};
+
+export const submitComment = async (
+  postId: string,
+  authorId: string,
+  content: string
+) => {
+  try {
+    return await addComment(content, authorId, postId);
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+export const removeComment = async (commentId: string) => {
+  try {
+    return await deleteComment(commentId);
+  } catch (error) {
+    return { error: error };
+  }
+};
