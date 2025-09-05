0d76099569a71fc9b6765dd065d9d1b09a734c82
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-05 09:05:09 +0800

    Styled

--- src/app/(components)/CommentsSection.tsx
+++ src/app/(components)/CommentsSection.tsx
@@ -87,35 +87,41 @@ export default function CommentsSection({
       </form>
       <div>
         {comments.map((comment) => (
-          <div key={comment.id} className="border-b mb-2">
+          <div key={comment.id}>
             <Link
               href={`/user/${comment.author.id}`}
-              className="flex items-center gap-2 cursor-pointer max-w-fit"
+              className="cursor-pointer grid grid-cols-[max-content_1fr]"
             >
               <SmallProfilePicture user={comment.author} />
-              <p>{comment.author.name}</p>
-            </Link>
-            <p>{comment.content}</p>
-            <div className="flex gap-2">
-              <p>{new Date(comment.createdAt).toDateString()}</p>
-              {user && user.id === comment.author.id && (
-                <button
-                  onClick={() => deleteComment(comment.id)}
-                  className="cursor-pointer bg-red-600 hover:bg-red-800 rounded"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-8 h-6"
+              <div className="flex gap-2 items-center ml-2">
+                <p>{comment.author.name}</p>
+                <p>{new Date(comment.createdAt).toDateString()}</p>
+                {user && user.id === comment.author.id && (
+                  <button
+                    type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      deleteComment(comment.id);
+                    }}
+                    className="cursor-pointer bg-red-600 hover:bg-red-800 rounded"
                   >
-                    <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                  </svg>
-                </button>
-              )}
-            </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-8 h-6"
+                    >
+                      <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                  </button>
+                )}
+              </div>
+
+              <p className="col-start-2 ml-2">{comment.content}</p>
+            </Link>
           </div>
         ))}
       </div>
