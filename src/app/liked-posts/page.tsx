64aebb4734752e910faf29ca5666bdab7f8d17a0
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-08 10:17:00 +0800

    Created LikedPostsPage()

---
+++ src/app/liked-posts/page.tsx
@@ -0,0 +1,25 @@
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+import { getLikedPosts, Post } from "@/models/postModels";
+import PostList from "../(components)/PostList";
+
+export default async function LikedPostsPage() {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/");
+  }
+
+  const likedPosts: Post[] = await getLikedPosts(session.user.id);
+
+  if (!likedPosts || likedPosts.length === 0) {
+    return <div>You have no posts liked.</div>;
+  }
+
+  return (
+    <div>
+      <PostList posts={likedPosts} route={"/liked-posts"} />
+    </div>
+  );
+}
