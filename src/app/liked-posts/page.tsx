fef8713d228fb3c65f9e40a9a4ff6cc805004239
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-10 10:47:03 +0800

    Added query parameter to getLikedPosts()

--- src/app/liked-posts/page.tsx
+++ src/app/liked-posts/page.tsx
@@ -3,24 +3,32 @@ import { options } from "../api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import { getLikedPosts, Post } from "@/models/postModels";
 import PostList from "../(components)/PostList";
+import SearchBar from "../(components)/SearchBar";
 
-export default async function LikedPostsPage() {
+export default async function LikedPostsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
   const session = await getServerSession(options);
+  const { query } = await searchParams;
 
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/");
   }
 
-  const likedPosts: Post[] = await getLikedPosts(session.user.id);
-
-  if (!likedPosts || likedPosts.length === 0) {
-    return <div>You have no posts liked.</div>;
-  }
+  const likedPosts: Post[] = await getLikedPosts(session.user.id, query);
 
   return (
     <div>
       <h2 className="text-xl font-bold mb-4">Liked Posts</h2>
-      <PostList posts={likedPosts} route={"/liked-posts"} />
+
+      <SearchBar route={"/liked-posts"} placeholder={`Search Liked Posts`} />
+      {!likedPosts || !likedPosts.length ? (
+        <p className="text-center">No Liked Posts Found.</p>
+      ) : (
+        <PostList posts={likedPosts} route={"/liked-posts"} />
+      )}
     </div>
   );
 }
