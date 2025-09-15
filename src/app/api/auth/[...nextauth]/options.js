f3e2e59b0b11e5358169db23dcec504a2073bdcd
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-09-15 11:01:49 +0800

    Update

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -2,7 +2,7 @@ import { getUserByEmail, getUserById } from "@/models/userModels";
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
 import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+// import Google from "next-auth/providers/google";
 
 export const options = {
   providers: [
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,8 @@
 
 import { useActionState } from "react";
 import { createUser, UserFormState } from "../controllers/usersControllers";
+import BlueButton from "../(components)/BlueButton";
+import Link from "next/link";
 
 export default function RegisterPage() {
   const initialState: UserFormState = {
@@ -16,71 +18,82 @@ export default function RegisterPage() {
   );
 
   return (
-    <form action={formAction} className="flex flex-col gap-4">
-      <h2 className="text-xl font-bold">Register Page</h2>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          defaultValue={state.name}
-        />
-        {state?.errors.name && (
-          <p className="text-red-500">{state.errors.name}</p>
-        )}
-      </div>
+    <>
+      <form action={formAction} className="flex flex-col gap-4 mb-4">
+        <h2 className="text-xl font-bold">Register Page</h2>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            defaultValue={state.name}
+          />
+          {state?.errors.name && (
+            <p className="text-red-500">{state.errors.name}</p>
+          )}
+        </div>
 
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          defaultValue={state.email}
-        />
-        {state?.errors.email && (
-          <p className="text-red-500">{state.errors.email}</p>
-        )}
-      </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            defaultValue={state.email}
+          />
+          {state?.errors.email && (
+            <p className="text-red-500">{state.errors.email}</p>
+          )}
+        </div>
 
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-        {state?.errors.password && (
-          <p className="text-red-500">{state.errors.password}</p>
-        )}
-      </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+          {state?.errors.password && (
+            <p className="text-red-500">{state.errors.password}</p>
+          )}
+        </div>
 
-      <div>
-        <label htmlFor="confirmPassword">Confirm Your Password:</label>
-        <input
-          type="password"
-          name="confirmPassword"
-          id="confirmPassword"
-          className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-        />
-        {state?.errors.confirmPassword && (
-          <p className="text-red-500">{state.errors.confirmPassword}</p>
-        )}
-      </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Your Password:</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            className="border rounded p-2 w-full focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+          {state?.errors.confirmPassword && (
+            <p className="text-red-500">{state.errors.confirmPassword}</p>
+          )}
+        </div>
 
-      {isPending ? (
-        <button className="bg-gray-500 px-4 py-2 rounded cursor-pointer">
-          Submitting...
-        </button>
-      ) : (
-        <button className="bg-blue-500 px-4 py-2 rounded cursor-pointer">
-          Register
-        </button>
-      )}
-    </form>
+        {isPending ? (
+          <button className="bg-gray-500 px-4 py-2 rounded cursor-pointer">
+            Submitting...
+          </button>
+        ) : (
+          <button className="bg-blue-500 px-4 py-2 rounded cursor-pointer">
+            Register
+          </button>
+        )}
+      </form>
+      <p>Already Have An Account?</p>
+      <div className="flex gap-2 mt-4">
+        <Link href={"/api/auth/signin?callbackUrl=/"}>
+          <BlueButton>Login Here</BlueButton>
+        </Link>
+        {/* <Link href={"/api/auth/signin?callbackUrl=/"}>
+          <BlueButton>Login With Google</BlueButton>
+        </Link> */}
+      </div>
+    </>
   );
 }
