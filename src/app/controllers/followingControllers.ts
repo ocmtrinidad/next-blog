83469eedcbb0bd7d73aed99992d81f33d93df7b9
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-10-09 14:06:24 +0800

    Update

--- src/app/controllers/followingControllers.ts
+++ src/app/controllers/followingControllers.ts
@@ -1,10 +1,6 @@
 "use server";
 
-import {
-  createFollowing,
-  deleteFollowing,
-  getFollowings,
-} from "@/models/followingModels";
+import { createFollowing, deleteFollowing } from "@/models/followingModels";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -15,19 +11,19 @@ export const followUser = async (
 ) => {
   try {
     await createFollowing(followerId, followedId);
-    revalidatePath(`/user/${name}`);
-    redirect(`/user/${name}`);
   } catch (error) {
     console.log(error);
   }
+  revalidatePath(`/user/${name}`);
+  redirect(`/user/${name}`);
 };
 
 export const unfollowUser = async (followerId: string, name: string) => {
   try {
     await deleteFollowing(followerId);
-    revalidatePath(`/user/${name}`);
-    redirect(`/user/${name}`);
   } catch (error) {
     console.log(error);
   }
+  revalidatePath(`/user/${name}`);
+  redirect(`/user/${name}`);
 };
