ecaf7a04254ccfd8a31acb0aa771e89865cf254e
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-10-11 08:13:43 +0800

    Imported <BlueButton>

--- src/app/(components)/FollowButton.tsx
+++ src/app/(components)/FollowButton.tsx
@@ -2,6 +2,7 @@
 
 import { followUser } from "../controllers/followingControllers";
 import { usePathname } from "next/navigation";
+import BlueButton from "./BlueButton";
 
 export default function FollowButton({
   followerId,
@@ -16,9 +17,7 @@ export default function FollowButton({
       className="flex items-center"
       onClick={() => followUser(followerId, followedId, pathname)}
     >
-      <button className="max-h-fit flex-1 bg-blue-500 px-2 py-1 rounded cursor-pointer hover:bg-blue-700">
-        Follow
-      </button>
+      <BlueButton>Follow</BlueButton>
     </div>
   );
 }
