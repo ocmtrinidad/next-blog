330b4938d02cc0dcafaf5f524473c91bcc18e8ec
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-10-15 13:35:36 +0800

    Styling

--- src/app/(components)/BlueButton.tsx
+++ src/app/(components)/BlueButton.tsx
@@ -4,7 +4,7 @@ export default function BlueButton({
   children: React.ReactNode;
 }) {
   return (
-    <button className="bg-blue-500 px-2 py-1 rounded cursor-pointer hover:bg-blue-700 h-full">
+    <button className="bg-blue-500 px-2 py-1 rounded cursor-pointer hover:bg-blue-700 h-full w-full">
       {children}
     </button>
   );
