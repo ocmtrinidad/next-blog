ebe63f59328637257950b775f0160c8b5828b919
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-10-21 12:19:16 +0800

    Styling

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -8,33 +8,37 @@ export default async function Navbar() {
   const session = await getServerSession(options);
 
   return (
-    <nav className="flex gap-2 items-center p-4 border-b">
-      <Link href={"/"} className="flex">
-        <BlueButton>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-8 h-6"
-          >
-            <path d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-          </svg>
-        </BlueButton>
-      </Link>
-      {session ? (
-        <Sidebar name={session.user.name} />
-      ) : (
-        <div className="flex justify-between items-center gap-2">
-          <Link href={"/register"}>
-            <BlueButton>Register</BlueButton>
-          </Link>
-          <Link href={"/api/auth/signin?callbackUrl=/"}>
-            <BlueButton>Login</BlueButton>
-          </Link>
-        </div>
-      )}
+    <nav className="flex justify-between p-4 border-b">
+      <div className="flex gap-2 items-center">
+        <Link href={"/"} className="flex">
+          <BlueButton>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-8 h-6"
+            >
+              <path d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+          </BlueButton>
+        </Link>
+        {session ? (
+          <Sidebar name={session.user.name} />
+        ) : (
+          <div className="flex justify-between items-center gap-2">
+            <Link href={"/register"}>
+              <BlueButton>Register</BlueButton>
+            </Link>
+            <Link href={"/api/auth/signin?callbackUrl=/"}>
+              <BlueButton>Login</BlueButton>
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <h2 className="text-2xl font-bold">ADMIN USER</h2>
     </nav>
   );
 }
